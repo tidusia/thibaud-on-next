cc917e42e8a210aa1d16d31086a49d883ec2f6ad
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2020-04-08 11:14:49 +0200

    Set up tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from "next";
 
-const Home: NextPage = () => <h1>Hello world!</h1>;
+const Home: NextPage = () => (
+  <h1 className={`text-center font-black text-2xl m-4 p-12`}>Hello world!</h1>
+);
 
 export default Home;
