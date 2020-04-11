e980a9376542cbcf4bd5d386fb09b50f3092ffe5
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2020-04-11 20:58:58 +0200

    Create Testimonial component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,19 @@ import { NextPage } from "next";
 
 import HomeIntro from "../components/HomeIntro";
 import ShowCase from "../components/ShowCase";
+import Testimonial from "../components/Testimonial";
 
 const Home: NextPage = () => (
   <div>
     <HomeIntro />
     <ShowCase />
+
+    <Testimonial
+      picture="/images/jerome-velociter.jpg"
+      name="Jérôme Vélociter"
+      position="Lead-Dev, Agricool"
+      content="Thibaud est un excellent intégrateur et développeur React, qui fait des choix pragmatiques et les communique clairement. Ce fut un plaisir de travailler avec lui sur la nouvelle version de notre site (Next.js + Prismic.io). Si l'occasion se présente, nous solliciterons ses services à nouveau dans le futur les yeux fermés."
+    />
   </div>
 );
 
