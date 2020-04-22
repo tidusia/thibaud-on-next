cfd7b4d051a49b32f6e1f574f9029396a395d486
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2020-04-22 11:53:46 +0200

    Try simplification of next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  exportTrailingSlash: true,
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
     });
     return config;
-  },
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-    };
   },
 };
