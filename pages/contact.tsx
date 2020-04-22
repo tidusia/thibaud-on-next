51b205a7860217cab78f983a6723e834534cef58
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2020-04-22 13:50:52 +0200

    Add missing field for Netlify Forms feature

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -52,11 +52,13 @@ const Contact: NextPage = () => (
         <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
           <div className="max-w-lg mx-auto lg:max-w-none">
             <form
-              method="POST"
+              method="post"
               name="contact"
               className="grid grid-cols-1 row-gap-6"
               data-netlify="true"
+              data-netlify-honeypot="bot-field"
             >
+              <input type="hidden" name="form-name" value="contact" />
               <div>
                 <label htmlFor="full_name" className="sr-only">
                   Nom complet
