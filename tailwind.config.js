e72c435cd20fe5724f089fb5bdbd9ba3bf0996b5
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2020-12-06 21:51:26 +0100

    Clean tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
+  future: {},
   darkMode: false,
   purge: {
-    // enabled: true,
     content: [
       "./pages/**/*.{js,jsx,ts,tsx}",
       "./components/**/*.{js,jsx,ts,tsx}",
