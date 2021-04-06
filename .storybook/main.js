2b4e8c3b28b339d4f628635515b11aa1b3617ace
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2021-04-06 14:02:20 +0200

    #28 Setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../components/**/*.stories.@(mdx|js|jsx|ts|tsx)",
+    "../pages/**/*.stories.@(mdx|js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
