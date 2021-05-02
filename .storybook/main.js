fe48a674c2452275df3a1287edb8b21cf1c505b2
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2021-05-02 20:30:06 +0200

    #50 Enables CSS Modules in Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     "../components/**/*.stories.@(mdx|js|jsx|ts|tsx)",
     "../pages/**/*.stories.@(mdx|js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  presets: [path.resolve(__dirname, "./next-preset.js")],
 };
---
+++ .storybook/next-preset.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+// Configuration found in this great gist : https://gist.github.com/justincy/b8805ae2b333ac98d5a3bd9f431e8f70#file-next-preset-js
+module.exports = {
+  webpackFinal: async (baseConfig, options) => {
+    // Modify or replace config. Mutating the original reference object can cause unexpected bugs.
+    const { module = {} } = baseConfig;
+
+    const newConfig = {
+      ...baseConfig,
+      module: {
+        ...module,
+        rules: [...(module.rules || [])],
+      },
+    };
+
+    //
+    // CSS Modules
+    // Many thanks to https://github.com/storybookjs/storybook/issues/6055#issuecomment-521046352
+    //
+
+    // First we prevent webpack from using Storybook CSS rules to process CSS modules
+    newConfig.module.rules.find(
+      (rule) => rule.test.toString() === "/\\.css$/",
+    ).exclude = /\.module\.css$/;
+
+    // Then we tell webpack what to do with CSS modules
+    newConfig.module.rules.push({
+      test: /\.module\.css$/,
+      include: path.resolve(__dirname, "../components"),
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1,
+            modules: true,
+          },
+        },
+      ],
+    });
+
+    return newConfig;
+  },
+};
