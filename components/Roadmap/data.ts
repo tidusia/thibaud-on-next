a01fe6dc40438ddb8ed3ff28d192cb276f249dc3
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2022-01-21 16:30:08 +0100

    feat(roadmap): continue adding data for the roadmap

--- components/Roadmap/data.ts
+++ components/Roadmap/data.ts
@@ -2,14 +2,50 @@ import { Domain } from "./index";
 import { totalNbOfProjects } from "../../data/projects";
 import hoursWorking from "../../data/hours-working";
 
+/**
+ * Sources :
+ * - Clients
+ * - Workshops (codeSchool, Opquast, CodeWars)
+ * - Livres (Kindle, ebooks en local, livres physiques)
+ * - OSS Github
+ */
 export const DOMAINS: Array<Domain> = [
   {
     title: "HTML",
     subTitle: "Franca lingua du Web",
+    description: `Le HTML est beaucoup plus riche et vaste que ce qu'on pourrait imaginer au premier abord. Je suis toujours surpris d'y découvrir des nouvelles choses régulièrement.
+
+Je me rappelle encore bien mes premiers pas dans le développement avec ce langage en 2013, pour créer le site d'une association.`,
     skills: [
       {
         type: "client",
-        content: `Utilisé dans tous mes projets clients depuis 2015, soit ${totalNbOfProjects} projets actuellement pour un total d'environs ${hoursWorking} heures de pratique.`,
+        content: `Utilisé dans tous mes projets clients depuis 2015, **soit ${totalNbOfProjects} projets** actuellement pour un total d'environs **${hoursWorking} heures de pratique**, sans compter les projets perso et la formation.`,
+      },
+      {
+        type: "book",
+        content:
+          "**HTML5, une référence pour le développeur web** - _Rodolphe Rimelé_",
+      },
+      {
+        type: "book",
+        content: "**HTML5 pour les web designers** - _Jeffrey Zeldman_",
+      },
+      {
+        type: "book",
+        content:
+          "**Réalisez votre site web avec HTML5 et CSS3** - _Mathieu Nebra_",
+      },
+      {
+        type: "formation",
+        content: "CodeSchool - Parcours complet HTML/CSS",
+      },
+      {
+        type: "formation",
+        content: "Frontend Masters - Complete intro to Web development v2",
+      },
+      {
+        type: "formation",
+        content: "Frontend Masters - HTML Email Development v2",
       },
     ],
   },
@@ -22,5 +58,108 @@ export const DOMAINS: Array<Domain> = [
         content: `Utilisé dans tous mes projets clients depuis 2015, soit ${totalNbOfProjects} projets actuellement pour un total d'environs ${hoursWorking} heures de pratique.`,
       },
     ],
+  },
+  {
+    title: "JavaScript",
+    subTitle: "Le langage le plus utilisé au monde (à part l'anglais)",
+    skills: [
+      {
+        type: "formation",
+        content: "Frontend Masters - Getting started with JavaScript v2",
+      },
+    ],
+  },
+  {
+    title: "Version Control Systems",
+    subTitle: "Et toi, tu git push ?",
+    skills: [],
+  },
+  {
+    title: "CSS Architecture",
+    subTitle: "CSS c'est simple, tant que c'est bien cadré...",
+    skills: [],
+  },
+  {
+    title: "CSS Preprocessors",
+    subTitle: "Des boosters parfois essentiels",
+    skills: [],
+  },
+  {
+    title: "Linters et Formatters",
+    subTitle: "Du joli code mignon tout plein",
+    skills: [],
+  },
+  {
+    title: "React",
+    subTitle: "Framework React et son écosystème, ses meta-framework",
+    skills: [],
+  },
+  {
+    title: "Modern CSS",
+    subTitle: "Il y a du nouveau dans le game",
+    skills: [],
+  },
+  {
+    title: "CSS Frameworks",
+    subTitle: "Parce que gagner du temps, c'est cool",
+    skills: [],
+  },
+  {
+    title: "Testing web apps",
+    subTitle: "Certainement mon sujet préféré",
+    skills: [],
+  },
+  {
+    title: "Type Checkers",
+    subTitle: "On n'est jamais assez sûr",
+    skills: [],
+  },
+  {
+    title: "Best practices",
+    subTitle: "Comment s'assurer d'un travail de qualité",
+    skills: [
+      {
+        type: "formation",
+        content: "Frontend Masters - Modern Search Engine Optimization (SEO)",
+      },
+      // Opquast
+      // Lighthouse
+    ],
+  },
+  {
+    title: "Server Side Rendering",
+    subTitle: "On revient aux classiques",
+    skills: [],
+  },
+  {
+    title: "GraphQL",
+    subTitle: "De la requête optimisée",
+    skills: [],
+  },
+  {
+    title: "Containers",
+    subTitle: "🐳",
+    skills: [
+      {
+        type: "formation",
+        content:
+          "Frontend Masters - Complete intro to Containers (feat. Docker)",
+      },
+    ],
+  },
+  {
+    title: "Static Site Generators",
+    subTitle: "Qui dit Jamstack, dit Jamtax !",
+    skills: [],
+  },
+  {
+    title: "Mobile Applications",
+    subTitle: "La quête du Graal, et des Stores",
+    skills: [],
+  },
+  {
+    title: "Freelancing",
+    subTitle: "",
+    skills: [],
   },
 ];
--- components/Roadmap/index.tsx
+++ components/Roadmap/index.tsx
@@ -1,7 +1,13 @@
-import { CheckIcon, SparklesIcon } from "@heroicons/react/outline";
+import {
+  CheckIcon,
+  SparklesIcon,
+  BookOpenIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/outline";
 import clsx from "clsx";
 import Link from "next/link";
 import ROUTES from "../../data/routes";
+import ReactMarkdown from "react-markdown";
 
 export type Skill = {
   type?: "client" | "perso" | "formation" | "book" | "article";
@@ -34,10 +40,13 @@ export default function Roadmap({ className, domains }: Props): JSX.Element {
             <span className="text-gray-900">Mes compétences </span>
             <span className="text-blue-600">en détails</span>
           </h1>
-          <p className="mt-5 max-w-4xl text-xl text-gray-500">
+          <p className="mt-5 max-w-3xl text-xl text-gray-500">
             Cette page me permets de centraliser les différents savoirs que j'ai
             pu obtenir avec le temps, et de retracer l'origine de ces
-            compétences (projets clients, formations, livres).
+            compétences.
+            <br className="mb-4" />
+            La liste ne se veut pas exhaustive, j'y indique les éléments les
+            plus importants pour faciliter la lecture.
           </p>
         </div>
         <Link href={ROUTES.contact.href}>
@@ -47,48 +56,66 @@ export default function Roadmap({ className, domains }: Props): JSX.Element {
         </Link>
       </div>
 
-      {domains.map((domain) => (
+      {domains.map((domain, index) => (
         <div
           key={domain.title}
-          className="border-t border-gray-200 pt-16 xl:grid xl:grid-cols-3 xl:gap-x-8"
+          className={clsx(
+            "border-t border-gray-200 py-16 lg:flex lg:gap-x-16",
+            index % 2 === 1 && "flex-row-reverse",
+          )}
         >
-          <div>
-            <h2 className="text-base font-semibold text-blue-600 uppercase tracking-wide">
+          <div className="flex-1 max-w-md">
+            <h2
+              lang="en"
+              className="text-base font-semibold text-blue-600 uppercase tracking-wide"
+            >
               {domain.title}
             </h2>
             <p className="mt-2 text-3xl font-extrabold text-gray-900">
               {domain.subTitle}
             </p>
             {!!domain.description && (
-              <p className="mt-4 text-lg text-gray-500">{domain.description}</p>
+              <ReactMarkdown className="mt-4 text-lg text-gray-500 whitespace-pre-line">
+                {domain.description}
+              </ReactMarkdown>
             )}
           </div>
-          <div>
-            <ul
-              role="list"
-              className="mt-4 sm:mt-8 md:mt-10 md:grid md:grid-cols-2 md:gap-x-8 xl:mt-0 xl:col-span-2"
-            >
-              {domain.skills.map((skill, index) => (
-                <li key={index} className="py-4 flex border-b border-gray-200">
-                  {skill.type === "client" && (
-                    <SparklesIcon
-                      className="flex-shrink-0 h-6 w-6 text-yellow-400"
-                      aria-hidden="true"
-                    />
-                  )}
-                  {skill.type === undefined && (
-                    <CheckIcon
-                      className="flex-shrink-0 h-6 w-6 text-green-500"
-                      aria-hidden="true"
-                    />
-                  )}
-                  <span className="ml-3 text-base text-gray-500">
-                    {skill.content}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <ul role="list" className="flex-1 mt-4 sm:mt-8 md:mt-10 xl:mt-0">
+            {domain.skills.map((skill, index) => (
+              <li
+                key={index}
+                className="py-4 flex border-b last:border-0 border-gray-200"
+              >
+                {skill.type === "client" && (
+                  <SparklesIcon
+                    className="flex-shrink-0 h-6 w-6 text-yellow-400"
+                    aria-hidden="true"
+                  />
+                )}
+                {skill.type === "book" && (
+                  <BookOpenIcon
+                    className="flex-shrink-0 h-6 w-6 text-red-500"
+                    aria-hidden="true"
+                  />
+                )}
+                {skill.type === "formation" && (
+                  <AcademicCapIcon
+                    className="flex-shrink-0 h-6 w-6 text-cyan-500"
+                    aria-hidden="true"
+                  />
+                )}
+                {skill.type === undefined && (
+                  <CheckIcon
+                    className="flex-shrink-0 h-6 w-6 text-green-500"
+                    aria-hidden="true"
+                  />
+                )}
+                <span className="ml-3 text-base">
+                  <ReactMarkdown>{skill.content}</ReactMarkdown>
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
