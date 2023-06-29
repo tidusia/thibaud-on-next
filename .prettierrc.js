873c6a7a8462014a6100f6777dea161993b261ac
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2023-06-29 17:03:11 +0200

    chore(deps): remove @tidusia/prettier-config, prefer to put config in project

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,19 @@
-module.exports = require("@tidusia/prettier-config");
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+};
