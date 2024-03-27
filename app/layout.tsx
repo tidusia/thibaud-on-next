4da76a9a5184c102b9416c7d9b74fc2619dcb305
Thibaud Duthoit <contact@thibaud-duthoit.fr>
2024-03-27 10:57:16 +0100

    refactor(app-router): create main app layout

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
